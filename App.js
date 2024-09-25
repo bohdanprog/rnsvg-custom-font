139c7b66f08a64071ba21587b2f4babb09be8fbf
bohdanprog <bohdan.artiukhov@swmansion.com>
2024-09-25 08:38:08 +0200

    feat: add example with custom font

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from "react-native";
+import Svg, { Text } from "react-native-svg";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    "SFProRounded-Regular": require("./assets/fonts/SFProRounded-Regular.otf"),
+  });
+
+  console.log(loaded, "loaded");
+  console.log(error, "error");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      {loaded ? (
+        <Svg width={"100%"} height={"100%"}>
+          <Text
+            fill={"black"}
+            fontSize={24}
+            fontWeight={600}
+            fontFamily={"SFProRounded-Regular"}
+            fontFeatureSettings={"tnum"}
+            x="20"
+            y="200"
+            textAnchor="start"
+          >
+            {"-+1!2@3#4$5%6^7&8*9(0)+-"}
+          </Text>
+        </Svg>
+      ) : (
+        <></>
+      )}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
